f6c2e521bd20e9dce5175237acf1f5a5c6761fc8
osiol_486 <51462326+osiol486@users.noreply.github.com>
2024-11-09 18:31:12 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+EMBED_COLOR_XXXX = 
+EMBED_COLOR_XXX = 
+EMBED_COLOR_XXX = 
+
+COMMAND_PREFIX = "!"
+
+ERROR_NO_PERMISSION_USER = "{ctx.author.mention} nie masz wystarczających uprawnień do wykonania tej komendy."
+ERROR_NO_PERMISSION_BOT = "{ctx.author.mention} nie mam wystarczających uprawnień do wykonania tej komendy."
